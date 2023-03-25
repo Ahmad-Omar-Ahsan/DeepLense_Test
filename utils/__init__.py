e2ba8d6d376b08bae120b7927e1366a382b89bb2
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2023-03-25 19:28:12 +0600

    Complete common task 1 dataset loader

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,5 +3,5 @@ from .loss import LabelSmoothingLoss
 from .optim import get_optimizer
 from .scheduler import WarmUpLR, get_scheduler
 from .train import train, evaluate
-from .dataset import get_train_valid_loader, get_test_loader
+from .dataset import get_train_val_dataloader_t1, get_test_dataloader_t1
 from .misc import seed_everything, count_params, get_model, calc_step, log
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,33 +1,58 @@
 import torch
 import os
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader,SubsetRandomSampler
 from torchvision.datasets import DatasetFolder
-from torchvision.transforms import transforms
 from typing import List
 import numpy as np
 
-def get_train_dataset_t1(root_dir : str, loader, transform: List):
-    train_dataset = DatasetFolder(root=root_dir,extensions=".npy", loader=loader,transform=transform)
-    return train_dataset
-
-
-def get_val_dataset_t1(root_dir : str, loader, transform: List):
-    val_dataset = DatasetFolder(root=root_dir,extensions=".npy", loader=loader,transform=transform)
-    return val_dataset
 
 def sample_loader(f :str):
     return np.load(f)
 
-"""
-Create train val from train folder
-treat val dir as test dir
-"""
+def get_dataset_t1(root_dir : str, loader = sample_loader, transform: List = None):
+    train_dataset = DatasetFolder(root=root_dir,extensions=".npy", loader=loader,transform=transform)
+    return train_dataset
+
+
+
+def get_train_val_dataloader_t1(root_dir : str, batch_size : int = 16):
+    train_val_dir = os.path.join(root_dir, 'train')
+
+    train_val_dataset = get_dataset_t1(train_val_dir,loader=sample_loader)
+    num_data = len(train_val_dataset)
+    indices = list(range(num_data))
+    split = int(num_data * 0.8) # 80% for training, 20% for validation
+    train_indices, val_indices = indices[:split], indices[split:]
+
+    # Define sampler for train and validation sets
+    train_sampler = SubsetRandomSampler(train_indices)
+    val_sampler = SubsetRandomSampler(val_indices)
+
+    # Define data loaders
+    train_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=train_sampler)
+    val_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=val_sampler)
+
+    return train_loader, val_loader
+
+
+def get_test_dataloader_t1(root_dir : str, batch_size : int = 16):
+    test_dir = os.path.join(root_dir, 'val')
+    test_dataset = get_dataset_t1(test_dir, loader=sample_loader)
+
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+    return test_loader
+
+
+
 
 if __name__=="__main__":
-    transform =transforms.Compose([   
-        transforms.ToTensor()
-    ])
-    train_dataset = get_train_dataset_t1('/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset/train', sample_loader, transform=transform)
-    print(len(train_dataset))
-    # x = np.load("/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset/train/no/1.npy")
-    # print(type(x[0][0][0]))
+    root_dir = '/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset'
+    
+    train_dataloader, val_dataloader = get_train_val_dataloader_t1(root_dir)
+    test_dataloader = get_test_dataloader_t1(root_dir)
+    
+    for i, (sample, label) in enumerate(train_dataloader):
+        print(sample.shape, label)
+        print(sample)
+        break
