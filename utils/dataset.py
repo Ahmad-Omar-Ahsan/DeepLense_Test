3a656fbd97107c9ca8229e9dfd256796d2df00c3
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2023-03-26 13:49:50 +0600

    Remove shuffle

--- utils/dataset.py
+++ utils/dataset.py
@@ -31,8 +31,8 @@ def get_train_val_dataloader_t1(root_dir : str, batch_size : int = 16):
     val_sampler = SubsetRandomSampler(val_idx)
 
     # Define data loaders
-    train_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=train_sampler,shuffle=True)
-    val_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=val_sampler,shuffle=True)
+    train_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=train_sampler)
+    val_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=val_sampler)
 
     return train_loader, val_loader
 
@@ -49,14 +49,14 @@ def get_test_dataloader_t1(root_dir : str, batch_size : int = 16):
 
 
 if __name__=="__main__":
-    root_dir = '/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset/train'
-    dataset = get_dataset_t1(root_dir=root_dir)
-    print(dataset.samples)
+    root_dir = '/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset'
+    # dataset = get_dataset_t1(root_dir=root_dir)
+    # print(dataset.samples)
     
-    # train_dataloader, val_dataloader = get_train_val_dataloader_t1(root_dir)
-    # test_dataloader = get_test_dataloader_t1(root_dir)
+    train_dataloader, val_dataloader = get_train_val_dataloader_t1(root_dir)
+    test_dataloader = get_test_dataloader_t1(root_dir)
     
-    # for i, (sample, label) in enumerate(train_dataloader):
-    #     print(sample.shape, label)
-    #     print(sample)
-    #     break
+    for i, (sample, label) in enumerate(train_dataloader):
+        print(sample.shape, label)
+       
+        break
