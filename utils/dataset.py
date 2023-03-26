7e3e9da3340ca95e6021e20e493bc7ac09ce545a
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2023-03-26 13:43:13 +0600

    Added shuffling, stratified split

--- utils/dataset.py
+++ utils/dataset.py
@@ -4,14 +4,14 @@ from torch.utils.data import DataLoader,SubsetRandomSampler
 from torchvision.datasets import DatasetFolder
 from typing import List
 import numpy as np
-
+from sklearn.model_selection import train_test_split
 
 def sample_loader(f :str):
     return np.load(f)
 
 def get_dataset_t1(root_dir : str, loader = sample_loader, transform: List = None):
-    train_dataset = DatasetFolder(root=root_dir,extensions=".npy", loader=loader,transform=transform)
-    return train_dataset
+    dataset = DatasetFolder(root=root_dir,extensions=".npy", loader=loader,transform=transform)
+    return dataset
 
 
 
@@ -19,18 +19,20 @@ def get_train_val_dataloader_t1(root_dir : str, batch_size : int = 16):
     train_val_dir = os.path.join(root_dir, 'train')
 
     train_val_dataset = get_dataset_t1(train_val_dir,loader=sample_loader)
-    num_data = len(train_val_dataset)
-    indices = list(range(num_data))
-    split = int(num_data * 0.95) # 95% for training, 5% for validation
-    train_indices, val_indices = indices[:split], indices[split:]
+    targets = train_val_dataset.targets
+    train_idx, val_idx= train_test_split(
+        np.arange(len(targets)),
+        test_size=0.05,
+        shuffle=True,
+        stratify=targets)
 
     # Define sampler for train and validation sets
-    train_sampler = SubsetRandomSampler(train_indices)
-    val_sampler = SubsetRandomSampler(val_indices)
+    train_sampler = SubsetRandomSampler(train_idx)
+    val_sampler = SubsetRandomSampler(val_idx)
 
     # Define data loaders
-    train_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=train_sampler)
-    val_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=val_sampler)
+    train_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=train_sampler,shuffle=True)
+    val_loader = DataLoader(train_val_dataset, batch_size=batch_size, sampler=val_sampler,shuffle=True)
 
     return train_loader, val_loader
 
@@ -47,12 +49,14 @@ def get_test_dataloader_t1(root_dir : str, batch_size : int = 16):
 
 
 if __name__=="__main__":
-    root_dir = '/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset'
+    root_dir = '/media/saitomar/Work/Projects/DeepLense_Test/task_1_dataset/dataset/train'
+    dataset = get_dataset_t1(root_dir=root_dir)
+    print(dataset.samples)
     
-    train_dataloader, val_dataloader = get_train_val_dataloader_t1(root_dir)
-    test_dataloader = get_test_dataloader_t1(root_dir)
+    # train_dataloader, val_dataloader = get_train_val_dataloader_t1(root_dir)
+    # test_dataloader = get_test_dataloader_t1(root_dir)
     
-    for i, (sample, label) in enumerate(train_dataloader):
-        print(sample.shape, label)
-        print(sample)
-        break
+    # for i, (sample, label) in enumerate(train_dataloader):
+    #     print(sample.shape, label)
+    #     print(sample)
+    #     break
