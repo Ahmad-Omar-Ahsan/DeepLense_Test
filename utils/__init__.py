b76e1eb1dd7e1b539915b0a894a4e7053039b8aa
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2023-03-29 14:11:53 +0600

    Finish task 2

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,4 @@ from .optim import get_optimizer
 from .scheduler import WarmUpLR, get_scheduler
 from .train import train, evaluate
 from .dataset import get_train_val_dataloader_t1, get_test_dataloader_t1, get_dataset_t2
-from .misc import seed_everything, count_params, get_model, calc_step, log
+from .misc import seed_everything, count_params, get_model, calc_step, log, generate_model_outputs
